d5201dc599f8da05e75a2701768e12d0902ebb99
赵祥 <939981212@qq.com>
2026-03-12 11:45:14 +0800

    fix: 配置 GitHub Pages 基础路径

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? '/threejs-globe/' : '/',
   optimizeDeps: {
     include: ['three']
   }

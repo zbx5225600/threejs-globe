88de5aa72f2a9c2ac59b05d05c110cfdc9bfe130
赵祥 <939981212@qq.com>
2026-03-12 08:11:31 +0800

    fix: 构建问题处理

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as THREE from 'three'
 import './index.css'
 import App from './App.jsx'
+
+// 将 THREE 暴露到全局作用域，供 @react-three/drei 使用
+window.THREE = THREE
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
